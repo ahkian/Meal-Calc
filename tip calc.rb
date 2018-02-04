d1dd1334a80229ab7e4f8032d7565d022f0f8ef3
ahkian <dbrazel4@gmail.com>
2018-02-04 15:25:47 -0500

    Update tip calc.rb

--- tip calc.rb
+++ tip calc.rb
@@ -1,8 +1,11 @@
-def tippy(price, tax_percent, tip_percent = 20)
-	tax = price * (tax_percent / 100)
+def tippy(price, tax_percent, tip_percent)
+  price = price.to_f
+  tax_percent = tax_percent.to_f
+  tip_percent = tip_percent.to_f
+	tax = (price * (tax_percent / 100.0))
 	sub_total = price + tax
-	tip = sub_total * (tax_percent / 100)
-	p "Your tip on a $#{sub_total} bill is #{tip}. This brings your bill to a total of $(#{sub_total} + #{tip}). "
+	tip = sub_total * (tip_percent / 100.0)
+	p "Your tip on a $#{sub_total} bill is $#{tip}. This brings your bill to a total of $#{sub_total + tip}. "
 end
 
 tippy(50, 10)
